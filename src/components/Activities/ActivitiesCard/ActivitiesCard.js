a1215c165bdbcb49f8e2b8a4980a732a9140774f
Eva Thorisdottir <heidrune97@gmail.com>
2020-03-04 02:38:03 +0100

    Added some comments

--- src/components/Activities/ActivitiesCard/ActivitiesCard.js
+++ src/components/Activities/ActivitiesCard/ActivitiesCard.js
@@ -17,3 +17,15 @@ const ActivitiesCard = props => {
 };
 
 export default ActivitiesCard;
+
+{
+  /* What's needed:
+-Onclick function to connect to calendar when its ready
+-fixes in margins and padding
+-rewriting CSS so its mobile first and responsive
+-fix "TODAY:" heading. Has to be changeable depending on user input, but we
+should probably leave it hardcoded for now.
+
+
+*/
+}
